bdfa9ac826578ffa0b9d835877914806eecf0f07
Priscila Manhaes <pmnhaes@gmail.com>
2016-05-30 19:24:15 -0300

    melhorias em referencias

---
+++ latex/referencias-old.rb
@@ -0,0 +1,80 @@
+Aguanno, K. (2004). Managing agile projects. Lakefield, Canada: Multi–Media Publications Inc.
+Amaral, D. C., Conforto, E. C., Benassi, J. L. G., & Araujo, C.. Gerenciamento ágil de projetos: aplicação em produtos inovadores. São Paulo: Saraiva, 2011.
+Andersen, E. (1996). Warning: Activity planning is hazardous to your project’s health! International Journal of Project Management, 14(2), 89–94.
+Andersen, E. S. (2006). Perspectives on projects. Proceedings of the PMI Research Conference 2006, Canada.
+Augustine, S. (2005). Managing agile projects. Upper Saddle River, NJ: Prentice Hall PTR.
+Baccarini, D. (1996). The concept of project complexity: A review. International Journal of Project Management, 14(4), 201–204.
+Barlow, J., Giboney, J., Keith, M., Wilson, D., Schuetzler, R., Lowry, P., & Vance, A. (2011). Overview and Guidance on Agile Development in Large Organizations. Communications of the Association for Information Systems, 29(2), 25-44.
+Batra, D., Xia, W., VanderMeer, D., & Dutta, K. (2010). Balancing Agile and Structured Development Approaches to Successfully Manage Large Distributed Software Projects: A Case Study from the Cruise Line Industry. Communications of the Association for Information Systems, 27(1),21.
+Beck, K. Extreme Programming Explained, Addison-Wesley, Pearson Education, 2000.
+Beck, K., Beedle, M., van Bennekum, A., Cockburn, A., Cunningham, W., et al. (2001). Manifesto for agile software development. Retrieved from http://agilemanifesto.org.
+Benassi, J. L. G., Junior, L. D. F., & Amaral, D. C. (2011). Evaluating methods for product vision with customers’ involvement to support agile project management. In: Proceedings of International Conference on Engineering Design, ICED, Technical University of Denmark.
+Benediktsson, O. & Dalcher, D. (2005). Estimating size in incremental software development projects, IEE Proceedings – Software, 152(6), 253–259.
+Biazzo, S. (2009). Flexibility, structuration, and simultaneity in new product development. Journal of Product Innovation Management, 26(3), 336–353.
+Boehm, B. (1988). A spiral model of software development and enhancement. Computer, 21(5), 61-72.
+Boehm, B., & Turner, R. (2003). Using risk to balance agile and plan-driven methods. Computer, 36(6), 57-66.
+Boehm, B., & Turner, R. (2004). Balancing agility and discipline: A guide for the perplexed. Boston, MA: Addison- Wesley.
+Boehm, B., Turner, R. Management challenges to implementing agile processes in traditional development organizations. Software, 22(5), 30-39, 2005.
+Cicmil, S., Hodgson, D., 2006. New possibilities for project management theory: a critical engagement. Project Management Journal 37 (3), 111–122 .
+Charvat, J. (2003). Project Management Methodologies: Selecting, Implementing, and Supporting Methodologies and Processes for Projects. Hoboken, NJ: John Wiley & Sons, Inc.
+CHIESA, Vittorio; FRATTINI, Federico. Exploring the differences in performance measurement between research and development: evidence from a multiple case study. R&D Management, v. 37, n. 4, p. 283-301, 2007.
+Chin, G. (2004). Agile project management: How to succeed in the face of changing project requirements. New York, NY: Amacom.
+Chin, C.M.M. & Spowage, A.C. (2010). Defining & Classifying Project Management Methodologies. PM World Today, 12(5).
+Cobb, C. G. (2011). Making sense of agile project management: Balancing control and agility. Hoboken, NJ: John Wiley & Sons, Inc.
+Cockburn, A. Selecting a project’s methodology. IEEE Software, 17(4): 64−71, 2000.
+Cockburn, A. Agile Software Development, Addison-Wesley, Boston, 2002
+Cockburn, A. (2004). Crystal clear: A human-powered methodology for small teams. Boston, MA: Addison-Wesley Professional
+Cohen, G. (2010). Agile excellence for product managers: A guide to creating winning products with agile development teams. Silicon Valley, CA: Super Star Press.
+Cohn, M. (2005). Agile estimating and planning. New York, NY: Prentice Hall PTR
+Collyer, S., Warren, C., Hemsley, B., & Stevens, C. (2010). Aim, fire, aim—Project planning styles in dynamic environments. Project Management Journal, 41(4), 108-121.
+Conforto & Amaral, 2008
+Conforto, E. C., & Amaral, D. C. (2010). Evaluating an agile method for planning and controlling innovative projects. Project Management Journal, 41(2),73-80.
+Coram, M. & Bohner, S. (2005). The impact of agile methods on software project management. Proceedings of the 12th IEEE International Conference and Workshops on the Engineering of Computer–Based Systems, USA
+Dawson, R., & Dawson, C. (1998). Practical proposals for managing uncertainty and risk in project planning. International Journal of Project Management, 16(5), 299–310.
+DeCarlo, D. (2004). eXtreme Project Management. San Francisco: Jossey–Bass.
+DINSMORE, P. C., CABANIS-BREWIN, J. – AMA – Manual de Gerenciamento de Projetos – 1ª
+edição, Brasport – 2009
+Eder, S., Conforto, E. C., Schneltzler, J. P., Amaral, D. C., & Silva, S. L.. Estudo das práticas de gerenciamento de projetos voltadas para desenvolvimento de produtos inovadores. Revista Produto & Produção, 13(1). Edição Especial CBGDP , 2012.
+GERALDI, Joana G. et al. Innovation in project management: Voices of researchers. International Journal of Project Management, v. 26, n. 5, p. 586-589, 2008.
+Goodpasture, C. J. (2010). Project management the agile way: Making it work in the enterprise. Fort Lauderdale, FL: J. Ross Publishing.
+Gunasekaran, A., 1998. Agile manufacturing: Enablers and an implementation framework. International Journal of Production Research 36 (5), 1223–1247.
+Gunasekaran, A., Yusuf, Y., 2002. Agile manufacturing: A taxonomy of strategic and technological imperatives. International Journal of Production Research 40 (6), 1357– 1385.
+Haas, K. B. (2007). The blending of traditional and agile project management. PM World Today – May 2007, 9(5).
+Highsmith, J. (2000). Adaptive software development: A collaborative approach to managing complex systems. New York, NY: Dorset House.
+Highsmith, J. (2004). Agile project management: Creating innovative products. Boston, MA: Addison-Wesley.
+Hoek, R.I., Harrison, A., Christopher, M., 2001. Measuring agile capabilities in the supply chain. International Journal of Operations and Production Management 21 (1/2), 126– 147.
+ISO. ISO 21500 (2012) Guidance on project management. Suíça, Genebra, 2012.
+Kahkonen, T. (2004) Agile methods for large organizations-building communities of practice. Agile Development Conference, 2004, 2-10.
+Kasarda, J.D., Rondinelli, D.A., 1998. Innovative infrastructure for agile manufacturers. Sloan Management Review (Winter), 73–83.
+Kerzner, H. (2001). Strategic Planning for Project Management using Project Management Maturity Model. New York, NY: John Wiley & Sons.
+Larman, C., & Basili, V. R. (2003). Iterative and incremental developments: a brief history. Computer, 36(6),47-56.
+Leffingwell, D. (2007). Scaling software agility: Best practices for large enterprises. Boston, MA: Addison–Wesley, Pearson Eduaction Inc.
+Lenfle, S., 2008. Exploration and project management. International Journal of Project Management 25 (6), 469–478 .
+LIN, Ching-Torng; CHIU, Hero; CHU, Po-Young. Agility index in the supply chain. International Journal of Production Economics, v. 100, n. 2, p. 285-299, 2006.
+MacCormack, A., Verganti, R., & Iansiti, M. (2001). Developing products on internet time: The anatomy of a flexible development process. Management Science, 47(1), 133–150.
+Magdaleno, A. M., Werner, C. M. L., & Araujo, R. M. D. (2011). Reconciling software development models: A quasi-systematic review. Journal of Systems and Software, 85(2), 351-369.
+Maylor, H. (2001). Beyond the Gantt chart: Project management moving on. European Management Journal, 19(1), 92–100.
+Milosevic, D. & Patanakul, P. (2005). Standardized project management may increase development projects success. International Journal of Project Management, 23(3), 181-192.
+Nelson, K.M., Ghods, M. & Nelson, H.J. (1998). Measuring the effectiveness of a structured methodology: a comparative analysis. Proceedings of the Thirty-First Hawaii International Conference on System Sciences, Kohala Coast, HI, 1998, 492-499.
+Palmer, S. R., & Felsing, J. M. (2002). A practical guide to feature-driven development. New York, NY: Prentice Hall PTR. Smith, P. G. (2007). Flexible product development: Building agility for changing
+Poppendieck T, Poppendieck M. Lean software development: An agile toolkit for software development managers. Addison-Wesley, London UK, 2003.
+PMI, Project Management Institute. Guia PMBOK: um guia do conjunto de conhecimentos do gerenciamento de projetos (5. ed.). São Paulo:  Saraiva, 2014.
+Shenhar, A. J., & Dvir, D. (2007). Reinventing project management: The diamond approach to successful growth and innovation. Boston, MA: Harvard Business School Press.
+Schwaber, K. (1997). Scrum development process. In Business Object Design and Implementation (pp. 117-134). Springer London.
+Schwaber, K., & Beedle, M.. Agile software development with Scrum. New York, NY: Prentice Hall, 2001.
+Schwaber, K. (2004). Agile project management with SCRUM. Redmond, WA: Microsoft Press.
+Sliger, M., & Broderick, S. (2008). The software project manager’s bridge to agility. Boston, MA: Addison-Wesley Professional.
+Smith, P. G. (2007). Flexible product development: Building agility for changing markets. San Francisco, CA: Jossey-Bass.
+Stapleton, J. DSDM: The Method in Practice, Addison-Wesley, Reading, MA, 1997.
+Steffens, W., Martinsuo, M., & Artto, K. (2007). Change decisions in product development projects. International Journal of Project Management, 25(7), 702–713.
+Styhre A. The bureaucratization of the project manager function: The case of the construction industry. Int. J. Project Manage. 2006;24(3):271–6.
+Thomas, J. & Mullaly, M. (2008). Researching the Value of Project Management. Newtown Square, PA: Project Management Institute.
+Thomke, S., & Reinertsen, D. (1998). Agile product development: Managing development flexibility in uncertain environments. California Management Review, 41(1), 8–30.
+ Qumer, A., Henderson-Sellers, B. Measuring agility and adoptability of agile methods: a 4-dimensional analytical tool, Procs. IADIS International Conference Applied Computing 2006 (eds. N. Guimara ̃es, P. Isaias and A. Goikoetxea), IADIS Press, 2006, pp. 503–507.
+Verganti, R. (1999). Planned flexibility: Linking anticipation and reaction in product development projects. Journal of Product Innovation Management, 16(4), 363–376
+Virine, L. (2008). Adaptive Project Management. PM World Today – May 2008, 10(5).
+Williams, T. (1999). The need for new paradigms for complex projects. International Journal of Project Management, 17(5), 269–273.
+Williams, T. (2005). Assessing and moving on from the dominant project management discourse in the light of project overruns. IEEE Transactions on Engineering Management, 52(4), 497–508.
+Winter, M., Smith, C., Morris, P., & Cicmil, S. (2006). Directions for future research in project management: The main findings of a UK government- funded research network. International Journal of Project Management, 24(8), 638–649.
+Wysocki, R. K. (2009). Effective project management: Traditional, agile, extreme . Fifth edition. Hoboken, NJ: Wiley.
+YUSUF, Y. Y. et al. Agile supply chain capabilities: Determinants of competitive objectives. European Journal of Operational Research, v. 159, n. 2, p. 379-392, 2004.
