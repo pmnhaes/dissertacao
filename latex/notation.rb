aa5f4e71b52c62ee0b5d229ad1359a02fc34ca0b
Priscila Manhaes <pmnhaes@gmail.com>
2016-07-12 18:58:28 -0300

    notation

---
+++ latex/notation.rb
@@ -0,0 +1,5 @@
+EPIGRAFE
+
+“Os planos mais bem elaborados do mundo não valem o papel no qual foram escritos se você não conseguir realizar nada.” Ralph S. Larsen (ex-presidente do Conselho e CEO da Johnson & Johnson).
+
+--------------------------------------------------------------------------------
