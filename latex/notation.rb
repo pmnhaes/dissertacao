b068a01a4ab8b4fd7b607a2ca1d59334d4c9fa9c
Priscila Manhaes <pmnhaes@gmail.com>
2016-08-22 15:05:38 -0300

    novos objetivos

--- latex/notation.rb
+++ latex/notation.rb
@@ -1,5 +1,32 @@
-EPIGRAFE
-
 “Os planos mais bem elaborados do mundo não valem o papel no qual foram escritos se você não conseguir realizar nada.” Ralph S. Larsen (ex-presidente do Conselho e CEO da Johnson & Johnson).
 
 --------------------------------------------------------------------------------
+
+Metodologia
+- referencial bibliografico
+ - Conceito de Maturidade
+ - CMMI
+ - MPS-BR ???
+
+Estudo de Caso
+ - PICG
+ - EMBRAPII
+ - IFF
+ - Modelo EMBRAPII
+
+Comparação Modelo embrapii vs. CMMI
+
+-> Refazer Cronograma!
+-> Citar apendice (Análise bibliográfica) em justificativa
+
+--------------------------------------------------------------------------------
+- Melhorar Redação
+- Objetivos Não ficaram claros
+- Demonstrar Modelo a ser seguido no projeto
+(Modelo de Excelência Operacional EMBRAPII)
+- Como será feito o mapeamento
+
+--------------------------------------------------------------------------------
+
+DEPOIS DE TUDO:
+- Verificar corte de palavras!
