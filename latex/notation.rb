c4ec8ed2bc10b8f1af19dfad35e391bb5fc657f9
Priscila Manhaes <pmnhaes@gmail.com>
2016-08-26 14:56:40 -0300

    consideracoes

--- latex/notation.rb
+++ latex/notation.rb
@@ -1,32 +1,61 @@
-“Os planos mais bem elaborados do mundo não valem o papel no qual foram escritos se você não conseguir realizar nada.” Ralph S. Larsen (ex-presidente do Conselho e CEO da Johnson & Johnson).
+SEMPRE CONSIDERAR
+- Verificar/Melhorar Redação
+- Verificar corte de palavras!
 
---------------------------------------------------------------------------------
+REFAZER
+- Objetivos: não ficaram claros
+- Justificativa: citar apendice (Análise bibliográfica)
+- Cronograma:
+- Metodologia:
+ - Demonstrar Modelo a ser seguido no projeto
+(Modelo de Excelência Operacional EMBRAPII)
+  - Como será feito o mapeamento
 
-Metodologia
+METODOLOGIA
 - referencial bibliografico
  - Conceito de Maturidade
  - CMMI
  - MPS-BR ???
 
-Estudo de Caso
+ESTUDO DE CASO
  - PICG
  - EMBRAPII
  - IFF
  - Modelo EMBRAPII
+ - Comparação Modelo embrapii vs. CMMI
 
-Comparação Modelo embrapii vs. CMMI
-
--> Refazer Cronograma!
--> Citar apendice (Análise bibliográfica) em justificativa
-
---------------------------------------------------------------------------------
-- Melhorar Redação
-- Objetivos Não ficaram claros
-- Demonstrar Modelo a ser seguido no projeto
-(Modelo de Excelência Operacional EMBRAPII)
-- Como será feito o mapeamento
+ANOTAÇÕES:
+- Comparar outros modelos encontrados na literatura?
 
 --------------------------------------------------------------------------------
 
-DEPOIS DE TUDO:
-- Verificar corte de palavras!
+--------------------------------------------------------------------------------
+APENDICE
+
+“Os planos mais bem elaborados do mundo não valem o papel no qual foram escritos se você não conseguir realizar nada.” Ralph S. Larsen (ex-presidente do Conselho e CEO da Johnson & Johnson).
+
+--------------------------------------------------------------------------------
+
+MATURIDADE
+
+Maturidade é o desenvolvimento de sistemas e processos que são por
+natureza repetitivos e garantem uma alta probabilidade de que cada
+um deles seja um sucesso. Entretanto, processos e sistemas repetitivos
+não são, por si só, garantia de sucesso. Apenas aumentam a sua
+probabilidade. Harold Kerzner
+
+A maturidade é uma qualidade ou estado de amadurecimento. Se
+aplicarmos o conceito de maturidade em uma organização, podemos relacionar a
+maturidade com o estado no qual a organização está em perfeitas condições para
+alcançar seus objetivos
+
+Atualmente as organizações são avaliadas quanto ao nível de maturidade
+dentro de uma escala definida por cada um dos modelos de maturidade em gestão de
+projetos já propostos.
+
+Os modelos de maturidade partem da premissa que as organizações,
+pessoas e setores evoluem por meio de um processo contínuo de desenvolvimento ou
+crescimento em direção a uma maturidade mais avançada. Os modelos têm por
+finalidade auxiliar na elaboração de processos e execução de melhores práticas para
+que as organizações se desenvolvam de forma constante.
+>>> GP Justiça Federal
