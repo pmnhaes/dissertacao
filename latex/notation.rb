0cf96d8e5593b5db96f3a6e26089252fef1a2d3b
Priscila Manhaes <pmnhaes@gmail.com>
2016-08-31 22:33:39 -0300

    instituiçoes e maturidade

--- latex/notation.rb
+++ latex/notation.rb
@@ -3,6 +3,7 @@ SEMPRE CONSIDERAR
 - Verificar corte de palavras!
 
 REFAZER
+- introdução: Citar Patah & Carvalho 2012!
 - Objetivos: não ficaram claros
 - Justificativa: citar apendice (Análise bibliográfica)
 - Cronograma:
@@ -29,33 +30,67 @@ ANOTAÇÕES:
 
 --------------------------------------------------------------------------------
 
+PMI, PROJECT MANAGEMENT INSTITUTE. A Guide to the project management body of
+knowledge (PMBoK Guide). Forth Edition. Project Management Institute, Four Campus
+Boulevard, Newtown Square, 2008.
+
+PMI, PROJECT MANAGEMENT INSTITUTE. PMI today June 2009. Project Management
+Institute, Four Campus Boulevard, Newtown Square, 2009.
+
+PROJECT MANAGEMENT INSTITUTE. Organizational Project Management
+Maturity Model (OPM3) Knowledge Foundation. Pennsylvania: Project Management
+Institute. Inc, 2003.
+
+PROJECT MANAGEMENT INSTITUTE. Project Management Body of Knowledge. 1.
+ed. Pennsylvania: Project Management Institute. Inc, 1996.
+
+PROJECT MANAGEMENT INSTITUTE. Project Management Body of Knowledge. 2.
+ed. Pennsylvania: Project Management Institute. Inc, 2000.
+
+PROJECT MANAGEMENT INSTITUTE. Project Management Body of Knowledge. 3.
+ed. Pennsylvania: Project Management Institute. Inc, 2004.
+
+PROJECT MANAGEMENT INSTITUTE. Project Management Body of Knowledge. 4.
+ed. Pennsylvania: Project Management Institute. Inc, 2008.
+
+PROJECT MANAGEMENT INSTITUTE. PM Today. Pennsylvania: Project
+Management Institute. Inc, April, 2009-a.
+
+PROJECT MANAGEMENT INSTITUTE. About Us. Apresenta informações sobre o
+PMI. Disponível em http://www.pmi.org/AboutUs/Pages/Default.aspx. Acesso em 12
+fev. 2010.
+
+PROJECT MANAGEMENT INSTITUTE. PMI Chapters. Apresenta informações
+sobre o Chapters brasileiros. Disponível em
+http://www.pmi.org/GetInvolved/Pages/PMI-Chapters.aspx. Acesso em 12 mar.
+2010-a
+
+PMI. Um guia do conhecimento em Gerenciamento de Projetos (Guia PMBOK). EUA:
+Project Management Institute, 2013.
+
+_____. PMSURVEY.ORG 2014 Edition: relatório personalizado. 2014a. O relatório foi
+personalizado utilizando-se ‘nível = país’ e ‘região = Americas – South America – Brazil’.
+Disponível em: <www.pmsurvey.org>. Acesso em: 09 set. 2015.
+
+_____. PMSURVEY.ORG 2014 Edition: relatório personalizado. 2014b. O relatório foi
+personalizado utilizando-se ‘nível = país’ e ‘região = Americas – South America – Brazil’ em conjunto com o filtro 1 onde foi selecionada a pergunta ‘1 – qual o principal setor de atuação de sua organização?’ e as respostas ‘Governo Administração Direta’ e ‘Governo Administração Indireta’. Disponível em: <www.pmsurvey.org>. Acesso em: 09 set. 2015.
+
+_____. Pulse of the profession: capitando o valor do gerenciamento de projetos. PMI, 2015. Disponível em: <http://www.pmi.org/learning/pulse.aspx>. Acesso em: 11 set. 2015.
+
+PRADO, D. Fundamentos do Modelo Prado-MMGP. Disponível em:
+<http://www.maturityresearch.com/novosite/biblio/fundamentos-modelo-prado-mmgp.pdf>.
+Acesso em: 14 set. 2015.
+
+PRADO, D.; ANDRADE, C. E. Pesquisa de maturidade em gerenciamento de projetos de
+Archibald & Prado: relatório 2014. Governo: Administração Direta e Indireta. Parte A:
+indicadores. 2015a. Disponível em:
+<http://www.maturityresearch.com/novosite/2014/download/2-
+Governo/RelatorioMaturidade2014-Governo-Parte-A-Indicadores.pdf>. Acesso em: 20 ago.
+2015.
+
+PRADO, D. S.; ARCHIBALD, R. D. Gerenciamento de projetos para executivos: inclui
+portfólios e programas. Nova Lima: INDG Tecnologia e Serviços, 2004.
+
 --------------------------------------------------------------------------------
-APENDICE
-
-“Os planos mais bem elaborados do mundo não valem o papel no qual foram escritos se você não conseguir realizar nada.” Ralph S. Larsen (ex-presidente do Conselho e CEO da Johnson & Johnson).
 
 --------------------------------------------------------------------------------
-
-MATURIDADE
-
-Maturidade é o desenvolvimento de sistemas e processos que são por
-natureza repetitivos e garantem uma alta probabilidade de que cada
-um deles seja um sucesso. Entretanto, processos e sistemas repetitivos
-não são, por si só, garantia de sucesso. Apenas aumentam a sua
-probabilidade. Harold Kerzner
-
-A maturidade é uma qualidade ou estado de amadurecimento. Se
-aplicarmos o conceito de maturidade em uma organização, podemos relacionar a
-maturidade com o estado no qual a organização está em perfeitas condições para
-alcançar seus objetivos
-
-Atualmente as organizações são avaliadas quanto ao nível de maturidade
-dentro de uma escala definida por cada um dos modelos de maturidade em gestão de
-projetos já propostos.
-
-Os modelos de maturidade partem da premissa que as organizações,
-pessoas e setores evoluem por meio de um processo contínuo de desenvolvimento ou
-crescimento em direção a uma maturidade mais avançada. Os modelos têm por
-finalidade auxiliar na elaboração de processos e execução de melhores práticas para
-que as organizações se desenvolvam de forma constante.
->>> GP Justiça Federal
