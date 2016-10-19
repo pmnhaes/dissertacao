f4182abaa3b36f57d022b108421eb9dc46611532
Priscila Manhaes <pmnhaes@gmail.com>
2016-10-19 19:07:00 -0200

    rascunho inicial artigo latex

---
+++ artigo/anotacoes.rb
@@ -0,0 +1,95 @@
+--------------------------------------------------------------------------------
+Six decades of project management research: Thematic trends and future opportunities
+--------------------------------------------------------------------------------
+
+
+Abstract
+We find the research to be dominated by empirical and deterministic perspectives while non-deterministic research enquiry remains weak and sporadic.
+We contend that stronger focus on non-deterministic perspective and a methodological convergence is necessary for the research to meaningfully advance towards theory building, and discuss potential avenues for further research.
+
+Introduction
+Despite the large quantum and the diversity of PM research, the field lacks convergence in two key areas.
+First, a well subscribed stream of research on success or failure factors (Cooke-Davies, 2002; Jugdev and Müller, 2005; Ika, 2009; Müller and Jugdev, 2012) suggests an unfinished nature of the search for explanations of project performance.
+Second, several authors point to weak theoretic foundation of the discipline (Shenhar, 2001; Söderlund, 2004a; Cicmil et al., 2006; Smyth and Morris, 2007; Whitty and Maylor, 2009; Morris, 2010) and argue for reexamination of the present research agenda.
+
+RQ1: What themes characterize the evolution of PM research over the past decades?
+RQ2: How are these themes reflected in recent PM research?
+RQ3: What themes are trending presently? What directions for future research do they indicate?
+
+In a most comprehensive study so far, Pollack and Adler (2015) use software-based scientometric techniques to analyze 94,472 unique records taken from Scopus and ISI Web of Science over 1962–2012 and show clusters of top keywords used in the abstracts and their evolution over time. They analyze physical co-occurrence of terms to form semantic clusters which offer basis for thematic sense making.
+Kwak and Anbari (2009) examine 537 papers over 1950–2007 from 18 peer-reviewed and practitioner journals from 8 allied management areas such as OR/DS/OM/SCM, OB/HRM, IT/IS, etc.,excluding PM-focused journals.Their analysis shows strong decadal growth in PM research from 1980s in all areas, with Strategy/PPM as the leading area of growth.
+Artto et al. (2009) use bibliometric methods to study 1164 articles related to project management from 23 business journals over 1986 - 2006 and show ‘Product development’ and ‘Organization and product design’ as the top themes in PM research; and ‘Innovation,’ ‘Performance,’ and ‘New product development’ as the top keywords. However, by excluding PM-focused journals, it is not clear how the findings are applicable to mainstream PM research.
+
+Crawford et al. (2006) use linguistic analysis of keywords to study 1051 papers over 1994–2003 from International Journal of Project Management (IJPM) and Project Management Journal (PMJ). Using a classification scheme to associate a cluster of keywords with a priori topic definitions they show themes such as ‘Project evaluation and improvement’ rising in significance and ‘Quality management’ declining over the study period.
+
+>>> o que aconteceu após 2003 ???
+
+Kloppenborg and Opfer (2002) use keywords from abstracts to analyze 3554 articles from several databases over 1960–1999. Using frequency analysis, their study maps keywords to industries and to PMBOK® knowledge and process areas to find most frequently used terms: ‘Cost,’ ‘Time,’ ‘Quality,’ ‘Risk’ in the knowledge areas; ‘Plan,’ ‘Control’ in process areas; and ‘Construction,’ ‘Information systems’ in the industry domains.
+
+>>> We observe that most of the above studies use keywords or derived categories as proxies for themes and employ counting or scientometric methods. They generally do not extend to semantic analysis for deriving the thematic trends, nor propose models or classification frameworks to situate extant body of knowledge and to inform on future research directions. We argue that keywords by themselves are not sufficient to derive the themes or trends. Themes are derived from the contexts and meanings within which the keywords are used; and trends are temporal progressions of influence gathered by such themes within the academic and practitioner communities. Our study attempts to bring out the themes and trends through a two-part systematic review of literature.
+
+
+RESEARCH
+--------------------------------------------------------------------------------
+Part 1 addresses RQ1 (36 artigos)
+
+clusterS:
+Deterministic: Eleven articles reviewed deterministically grounded themes, prescriptive methodologies, or models having a priori conceptual, heuristic, or theoretic basis. Examples of such themes included scheduling, resource-constrained or time/cost constrained scheduling, control methods such as earned value analysis, etc.
+
+Explanations: Fifteen articles reviewed empirical studies seeking to establish linkages between variables of interest based on primary or secondary data. These articles aggregated the available evidence into prescriptions, models, or frameworks for utility to practitioners. Themes covered in this cluster included success or failure factors, leadership styles, human resource management practices, stakeholder theories in practice, and effective control practices.
+
+Non_deterministic: Five articles focused on themes dealing with non-deterministic aspects of the project phenomena. Typically, these themes addressed complexity, uncertainty, interdependence between project entities, managing risk etc., in the project phenomena.
+
+General_themes: Five articles examined themes of general interest such as typology of research in non PM-focused journals, PM research across industry sectors, distribution of research by PMBOK® areas etc.
+
+
+A closer look at the literature reviews delivers three insights:
+
+1. The evolution from deterministic to explanatory era can be viewed as a transition from a means orientation to an ends orientation. However, both eras share the assumption that project phenomena are essentially deterministic and tractable. The non-deterministic research relaxes the assumption of tractability.
+
+2. The three eras differ in their ontological assumptions. The deterministic era views the phenomena through a reductionist lens under the ‘hard’ paradigm (Pollack, 2007, p. 268), employs conceptual/analytical models, and delivers efficiency-focused methods such as optimization of cost or time. In contrast, the explanatory era sees divergence, and employs empirical methods yielding a large number of variables, factors, methods, and practices. The non-deterministic era with its emphasis on complexity theoretic constructs such as emergence and non-linear feedback loops (Benbya and McKelvey, 2006, ⁎Winter et al., 2006b and ⁎Whitty and Maylor, 2009) and project organization (Söderlund, 2004a) argues for a blend of empirical and conceptual approaches.
+
+3. The concurrence of the three eras from mid-90s and the growing thematic diversity suggests weak (ontological, epistemological, or methodological) convergence in PM research.
+
+RQ1: What themes characterize the evolution of PM research over the past decades?
+
+Deterministic: perspective, Project methods, Project strategy, and Knowledge management are the major themes over 2000–2015
+
+Explanatory: perspective addresses a large number of themes. These include (in the descending order of influence): Success factors, Performance management, Project methods, and Risk management. Other themes include Governance & control, Knowledge management, Stakeholder management, and Public private partnership.
+
+Non_deterministic: perspective is the smallest of the three, and deals with Risk management, Project complexity, and Project uncertainty as the main themes. Themes such as interdependence with project environments and external contexts are also addressed under the non-deterministic perspective; however these appear sporadically during the period.
+
+--------------------------------------------------------------------------------
+Part 2 addresses RQ2 and RQ3
+The steady decline in EACR in Fig. 5 indicates exhaustion of the older themes as well as weak influence of new themes; and prompts an examination of the research themes from the sample of highly cited articles.
+
+RQ2: How are these themes reflected in recent PM research?
+TABLE_2 => 3.2.1 cap
+
+
+RQ3: What themes are trending presently? What directions for future research do they indicate?
+
+Several authors have noted the atheoretic nature of the discipline and have stressed the need for greater theory-building effort. A review of 21 papers7 from our sample reveals three major prescriptions for research directions:
+
+1.
+Paradigmatic or methodological prescriptions include conceptualization of projects as multi-objects in open systems (Winter et al., 2006b); developing normative models to reflect current best practice e.g. critical chain (Maylor, 2001); adopting soft paradigm (Pollack, 2007); Situating research correctly within paradigms (Smyth and Morris, 2007); and Blending alternate approaches such as PMBOK® and Agile methods (Williams, 2005).
+2.
+Calls for several alternate perspectives such as Complexity in projects (Shenhar and Dvir, 1996, ⁎Williams, 1999, ⁎Williams, 2005, ⁎Shenhar, 2001, ⁎Pich et al., 2002, ⁎Winter et al., 2006b, ⁎Cicmil et al., 2006, ⁎Artto and Kujala, 2008 and ⁎Whitty and Maylor, 2009); Organizational or human characteristics and interactions (⁎Maylor, 2001, ⁎Söderlund, 2004a, ⁎Winter et al., 2006a, ⁎Winter et al., 2006b, ⁎Cooke-Davies et al., 2007, ⁎Shenhar and Dvir, 2007 and ⁎Whitty and Maylor, 2009); Knowledge, skills and learning (⁎Maylor, 2001, ⁎Cicmil et al., 2006 and ⁎Sauer and Reich, 2009); Social contexts and agendas (⁎Söderlund, 2004a, ⁎Sauer and Reich, 2009 and ⁎Morris, 2010); and Integration (⁎Maylor, 2001, ⁎Shenhar and Dvir, 2007 and ⁎Blomquist et al., 2010).
+3.
+These authors also suggest focus on research outcomes that include grounding of variables, factors, and measures; and suggest development of models, typologies and frameworks towards theory-building.
+
+
+--------------------------------------------------------------------------------
+CONCLUSION
+
+Project management literature is characterized by a rich tradition of multiple paradigms, perspectives, methodologies and streams of enquiry, and weak theories. There are very few studies that aggregate the diverse research streams into meaningful themes and to inform on further research directions.
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
